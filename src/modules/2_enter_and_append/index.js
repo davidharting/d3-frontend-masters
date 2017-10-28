357a33c7bd42bd9e66ed25f5018149aef0f8c7a6
David Harting <david@emplify.com>
2017-10-28 11:41:26 -0400

    [2 enter & append] Use enter() and append() to draw rects

--- src/modules/2_enter_and_append/index.js
+++ src/modules/2_enter_and_append/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3'
 
 class EnterAndAppend extends Component {
+  componentDidMount() {
+    const rectWidth = 50
+    const height = 200
+    var data = [100, 250, 175, 200, 120]
+    
+    const svg = d3.select('#svg-2')
+    svg.selectAll('.rect-2')
+      .data(data)
+      .enter().append('rect')
+      .attr('class', 'rect-2')
+      .attr('x', (d, i) => i * rectWidth)
+      .attr('y', d => height - d)
+      .attr('width', rectWidth)
+      .attr('height', d => d)
+      .attr('fill', 'blue')
+      .attr('stroke', '#fff')
+  }
+  
   render() {
-    return (
-      <svg />
-    )
+    return <svg id='svg-2' />
   }
 }
 
