643a355c6f72084d6d6a5f527bcb96b7c3824dba
David Harting <david@emplify.com>
2017-10-28 12:42:44 -0400

    [2] Data is bound, so you can dynamically respond to data

--- src/modules/2_enter_and_append/index.js
+++ src/modules/2_enter_and_append/index.js
@@ -3,12 +3,12 @@ import * as d3 from 'd3'
 
 class EnterAndAppend extends Component {
   componentDidMount() {
-    const rectWidth = 50
+    const rectWidth = 35
     const height = 200
-    var data = [100, 250, 175, 200, 120]
+    var data = [100, 250, 55, 200, 120, 60, 175]
     
     const svg = d3.select('#svg-2')
-    svg.selectAll('.rect-2')
+    const rects = svg.selectAll('.rect-2')
       .data(data)
       .enter().append('rect')
       .attr('class', 'rect-2')
@@ -16,7 +16,15 @@ class EnterAndAppend extends Component {
       .attr('y', d => height - d)
       .attr('width', rectWidth)
       .attr('height', d => d)
-      .attr('fill', 'blue')
+      .attr('fill', function determineFill(d) {
+        if (d <= 60) {
+          return 'lightblue'
+        }
+        if (d <= 150) {
+          return 'blue'
+        }
+        return 'purple'
+      })
       .attr('stroke', '#fff')
   }
   
