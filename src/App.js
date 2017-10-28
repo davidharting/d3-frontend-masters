79d5dfa58843b1f381493985e0ac7e887efce005
David Harting <david@emplify.com>
2017-10-28 11:37:35 -0400

    [2 enter & append] Add empty module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './assets/d3_logo.png';
 import './App.css';
 import SelectionAndData from './modules/1_selection_and_data';
+import EnterAndAppend from './modules/2_enter_and_append';
 
 class App extends Component {
   render() {
@@ -16,6 +17,8 @@ class App extends Component {
         </p>
         <h2>1. Selection and Data</h2>
         <SelectionAndData />
+        <h2>2. Enter and Append</h2>
+        <EnterAndAppend />
       </div>
     );
   }
---
+++ src/modules/2_enter_and_append/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class EnterAndAppend extends Component {
+  render() {
+    return (
+      <svg />
+    )
+  }
+}
+
+export default EnterAndAppend
