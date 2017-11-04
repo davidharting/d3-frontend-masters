9128f41939da698d87178f189a79b24c29b223e4
David Harting <david@emplify.com>
2017-11-04 15:17:40 -0400

    [3] Plot bar graph using scales

--- src/modules/3_scales_and_axes/index.js
+++ src/modules/3_scales_and_axes/index.js
@@ -24,8 +24,57 @@ import * as d3 from 'd3'
  *   .range([height, 0])
  */
 
+const data = [
+  { month: 'January', avgTemp: 34.5 },
+  { month: 'February', avgTemp: 39.9 },
+  { month: 'March', avgTemp: 51.4 },
+  { month: 'April', avgTemp: 62.9 },
+  { month: 'May', avgTemp: 73.5 },
+  { month: 'June', avgTemp: 82.1 },
+  { month: 'July', avgTemp: 85.6 },
+  { month: 'August', avgTemp: 83.7 },
+  { month: 'September', avgTemp: 77.4 },
+  { month: 'October', avgTemp: 65.6 },
+  { month: 'November', avgTemp: 51.6 },
+  { month: 'December', avgTemp: 39.2 }
+]
+
+const chartWidth = 500
+const chartHeight = 500
+const barWidth = chartWidth / data.length - 3
+
+const margin = {
+  top: 0,
+  bottom: 0,
+  left: 40,
+  right: 0
+}
+
+const xScale = d3.scaleBand()
+  .domain(data.map(data => data.month))
+  .range([margin.left, chartWidth - margin.right])
+
+const heightScale = d3.scaleLinear()
+  .domain(d3.extent(data, d => d.avgTemp))
+  // Remember, y=0 is the top of the SVG!
+  .range([0, chartHeight])
+
 class ScalesAndAxes extends Component {
   componentDidMount() {
+    const svg = d3.select('#svg-3')
+      .attr('width', chartWidth + margin.left + margin.right)
+      .attr('height', chartHeight + margin.top + margin.bottom)
+    
+    svg.selectAll('.rect-3')
+      .data(data).enter()
+      .append('rect')
+        .attr('x', d => xScale(d.month))
+        .attr('height', d => heightScale(d.avgTemp))
+        .attr('y', d => chartHeight - margin.bottom - heightScale(d.avgTemp))
+        .attr('width', barWidth)
+        .attr('fill', 'blue')
+        .attr('class', 'rect-3')
+        .attr('stroke', '#ffffff')
   }
   
   render() {
